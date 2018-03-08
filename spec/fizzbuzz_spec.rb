57fad79c8b5cace5c89f5450ca7bc6d9debcd491
marydomashneva <mary.domashneva@gmail.com>
2018-03-08 18:45:19 +0000

    Commiting fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,12 @@
+require 'fizzbuzz'
 describe "fizzbuzz" do
   it "takes a number and checks and its divisible by 3" do
-    expect(fizzbuzz(6)).to eq 'fizz'
+    expect(fizzbuzz(12)).to eq 'fizz'
+  end
+  it "take a number and check if it is devisible by 5" do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+  it "takes a number and checks if it is divisible by 5 and 3" do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
